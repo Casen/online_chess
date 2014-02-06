c69a9c1712547a6ded57abd79dad87b2c99e9f36
Casen <cdubd10@gmail.com>
2014-02-05 21:57:20 -0500

    adding basic chessboard

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ require('./config/passport')(passport); // pass passport for configuration
 app.configure(function() {
 
   // set up our express application
+  app.use(express.static(__dirname + '/public'));
   app.use(express.logger('dev')); // log every request to the console
   app.use(express.cookieParser()); // read cookies (needed for auth)
   app.use(express.bodyParser()); // get information from html forms
