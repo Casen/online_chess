961c00d76a2bd64a6cc8b45a3cfa84551fd5685b
Casen <cdubd10@gmail.com>
2014-07-14 21:57:53 -0400

    adding a build task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,52 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     browserify: {
-      'public/scripts/app.js': ['app/assets/scripts/*.js']
+      'build/scripts/app.js': ['app/assets/scripts/*.js']
+    },
+    copy: {
+      dev: {
+        files: [
+          {
+            expand: true,
+            cwd: 'app/assets/img',
+            src: ['**'],
+            dest: 'build/img/',
+            filter: 'isFile'
+          },
+          {
+            expand: true,
+            cwd: 'app/assets/scripts/vendor',
+            src: ['**/*.js'],
+            dest: 'build/scripts/',
+            filter: 'isFile'
+          },
+          {
+            expand: true,
+            cwd: 'app/assets/stylesheets/vendor',
+            src: ['**/*.css'],
+            dest: 'build/stylesheets/',
+            filter: 'isFile'
+          }
+        ]
+      }
+    },
+    less: {
+      dev: {
+        files: [
+          {
+            expand: true,
+            cwd: 'app/assets/stylesheets',
+            src: ['*.less'],
+            dest: 'build/stylesheets/',
+            filter: 'isFile',
+            ext: '.css'
+          }
+        ]
+      }
     }
   });
   grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.registerTask('build', ['browserify', 'copy:dev', 'less:dev']);
 };
