e412494b0d89161f59346fbd06ae8c84db13ba3c
Casen <cdubd10@gmail.com>
2014-07-28 17:27:52 -0400

    adding grunt-sequelize to manage migrations

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,9 @@
+var assert = require('assert');
+var config = require('./config');
+var env = process.env.NODE_ENV || 'local';
+var settings = config.getSettings({env: env});
+assert.notEqual(settings.ENV, 'common', 'Could not locate environment "' + env + '"');
+
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -44,10 +50,23 @@ module.exports = function(grunt) {
           }
         ]
       }
+    },
+    sequelize: {
+      options: {
+        migrationsPath: __dirname + '/migrations',
+        //The following is the sequelize config you're used to
+        dialect: settings.database.dialect,
+        username: settings.database.username,
+        password: settings.database.password,
+        database: settings.database.name,
+        host: settings.database.host,
+        port: settings.database.port
+      }
     }
   });
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-sequelize');
   grunt.registerTask('build', ['browserify', 'copy:dev', 'less:dev']);
 };
